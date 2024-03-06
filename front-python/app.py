ecfa318910fe085b4f1516c41c351e2e54538f23
Pierre-Yves Marec <Pierre-Yves.Marec@grenoble-inp.org>
2024-03-06 16:09:42 +0100

    Modification du front

--- front-python/app.py
+++ front-python/app.py
@@ -1,34 +1,27 @@
 import dash
-from dash import dcc 
-from dash import html
+from dash import dcc, html
 
 # Définir les feuilles de style externes
-external_stylesheets = ['https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css', './styles.css']
+external_stylesheets = ['https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css', './assets/styles.css']
 
 # Créer l'application Dash      
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 # Définir la mise en page de votre application avec une barre de navigation
-app.layout = html.Div(children=[
-    html.Nav(children=[
+app.layout = html.Div( children=[
+    html.Nav(className='navbar',children=[
         html.A('Accueil', href='#'),
         html.A('À propos', href='#'),
         html.A('Contact', href='#'),
-        # Ajoutez d'autres liens de navigation selon vos besoins
     ]),
     html.Div(children=[
         html.H1(children='Mon Application Dash'),
         html.Div(children='''
             Bienvenue sur ma première application Dash !
-        ''', className='description'),  # Utilisation de la classe CSS dans la div
-        # Ajoutez d'autres composants Dash selon vos besoins
+        ''', className='description'), 
     ], className='content')
 ])
-
-# Ajouter des callbacks si nécessaire
-# ...
 
 # Démarrer le serveur
 if __name__ == '__main__':
     app.run_server(debug=True)
- 
