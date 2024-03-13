a42b44cb7a866d69f3645bc04d42d81f73c31beb
Alan Meret <alan.meret@grenoble-inp.org>
2024-03-13 16:30:45 +0100

    ajout des fichiers python pour creer l'output json

--- front-python/data_provider.py
+++ front-python/data_provider.py
@@ -3,15 +3,15 @@ from datetime import datetime
 import json
 import os
 
-def _update_interest_rates(new_interest_rates, test_params):
-    test_params[Currencies][0][InterestRate] = new_interest_rates[REUR]
-    test_params[Currencies][1][InterestRate] = new_interest_rates[RGBP]
-    test_params[Currencies][2][InterestRate] = new_interest_rates[RJPY]
-    test_params[Currencies][3][InterestRate] = new_interest_rates[RINR]
+def _update_interest_rates(new_interest_rates, parametre_tester):
+    parametre_tester[Currencies][0][InterestRate] = new_interest_rates[REUR]
+    parametre_tester[Currencies][1][InterestRate] = new_interest_rates[RGBP]
+    parametre_tester[Currencies][2][InterestRate] = new_interest_rates[RJPY]
+    parametre_tester[Currencies][3][InterestRate] = new_interest_rates[RINR]
 
-def _update_covariance_matrix(int_date, test_params, nb_days=520):
-    covar = log_daily_rentas.iloc[max(0, int_date - nb_days):int_date, :]
-    test_params[CovarianceMatrix] = covar.cov().to_numpy().tolist()
+def _update_covariance_matrix(int_date, parametre_tester, nb_days=520):
+    covar_math = log_daily_rentas.iloc[max(0, int_date - nb_days):int_date, :]
+    parametre_tester[CovarianceMatrix] = covar_math.cov().to_numpy().tolist()
 
 def spots_builder_for_past(int_date):
     spots = []
@@ -24,24 +24,24 @@ def spots_builder_for_past(int_date):
         spots.append(spot)
     return spots
 
-def _update_past(int_date, test_params):
+def _update_past(int_date, parametre_tester):
     past = []
-    for const_date in test_params[Option][FixingDatesInDays][DatesInDays]:
+    for const_date in parametre_tester[Option][FixingDatesInDays][DatesInDays]:
         if const_date < int_date:
             past.append(spots_builder_for_past(const_date))
     past.append(spots_builder_for_past(int_date))
-    test_params[Past] = past
+    parametre_tester[Past] = past
 
-def _remove_offset(test_params):
-    dates = test_params[Option][FixingDatesInDays][DatesInDays]
+def _remove_offset(parametre_tester):
+    dates = parametre_tester[Option][FixingDatesInDays][DatesInDays]
     t_zero = dates[0]
     dates[:] = [date - t_zero for date in dates]
-    test_params[MathDate] -= t_zero
-    test_params[Option][MaturityInDays] -= t_zero
+    parametre_tester[MathDate] -= t_zero
+    parametre_tester[Option][MaturityInDays] -= t_zero
 
-def _verify_test_params(test_params):
-    mat_date = test_params[Option][MaturityInDays]
-    dates = test_params[Option][FixingDatesInDays][DatesInDays]
+def _verify_parametre_tester(parametre_tester):
+    mat_date = parametre_tester[Option][MaturityInDays]
+    dates = parametre_tester[Option][FixingDatesInDays][DatesInDays]
     if dates[-1] != mat_date:
         print(f"Last date doesn't match maturity. Last: {dates[-1]}, Maturity: {mat_date}", file=stderr)
         dates.append(mat_date)
@@ -51,21 +51,21 @@ def _conv_dates(option):
     const_dates = option[FixingDatesInDays][DatesInDays]
     option[FixingDatesInDays][DatesInDays] = [daily_dates_mapper(date) for date in const_dates]
 
-def provide_test_params(int_date, option):
+def provide_parametre_tester(int_date, option):
     _conv_dates(option)
-    test_params = get_test_parameters()
-    test_params[MathDate] = int_date
-    test_params[Option] = option
+    parametre_tester = get_test_parameters()
+    parametre_tester[MathDate] = int_date
+    parametre_tester[Option] = option
 
-    _update_past(int_date, test_params)
-    _update_interest_rates(taux_interet.iloc[int_date].to_dict(), test_params)
-    _update_covariance_matrix(int_date, test_params)
-    _remove_offset(test_params)
-    _verify_test_params(test_params)
-    return test_params
+    _update_past(int_date, parametre_tester)
+    _update_interest_rates(taux_interet.iloc[int_date].to_dict(), parametre_tester)
+    _update_covariance_matrix(int_date, parametre_tester)
+    _remove_offset(parametre_tester)
+    _verify_parametre_tester(parametre_tester)
+    return parametre_tester
 
-def provide_test_params_from_saved_option(int_date, option_num): 
-    return provide_test_params(int_date, get_preload_option_description(option_num))
+def provide_parametre_tester_from_saved_option(int_date, option_num): 
+    return provide_parametre_tester(int_date, get_preload_option_description(option_num))
 
 current_dir = os.path.abspath(os.getcwd())
 
@@ -78,4 +78,4 @@ date = daily_dates_mapper(date.strftime('%d-%m-%Y'))
 # Joindre le chemin absolu du répertoire actuel avec le nom du fichier de sortie
 output_path = os.path.join(current_dir, output_filename)
 with open(output_path, 'w') as f:
-    json.dump(provide_test_params_from_saved_option(date, 1), f, indent=4)
+    json.dump(provide_parametre_tester_from_saved_option(date, 1), f, indent=4)
