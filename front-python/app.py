2a2fb0a2a4c957490c5dc9f52f75b1e5d4de7a9c
Alan Meret <alan.meret@grenoble-inp.org>
2024-03-25 16:52:37 +0100

    date

--- front-python/app.py
+++ front-python/app.py
@@ -7,6 +7,8 @@ import subprocess
 import data_provider as dp
 import dash_table
 import json
+import dataframes as dataf
+from datetime import datetime
 
 # Définir les feuilles de style externes
 external_stylesheets = ['https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css', './assets/styles.css']
@@ -22,6 +24,18 @@ df['Date'] = pd.to_datetime(df['Date'])
 
 # Indices à afficher
 indices = ['EUROSTOXX50', 'FTSE100', 'MIB', 'NIKKEI', 'SENSEX']
+
+def math_date_to_real_date(math_date):
+    # Conversion de la première date d'option en format datetime
+
+    
+    first_option_dates = datetime.strptime(dataf._first_option_dates[0], "%d-%m-%Y")
+    index_ligne = df.index.get_loc(first_option_dates)
+    index_ligne = index_ligne + math_date
+    date = df['Date'][index_ligne]
+    # Calcul de la vraie date en ajoutant le nombre de jours écoulés depuis la première date d'option
+    return date
+
 
 # Callback pour afficher les courbes lorsque le bouton est cliqué
 @app.callback(Output('graph-container', 'children'),
@@ -64,17 +78,16 @@ def afficher_courbes(n_clicks, start_date, end_date):
 
 
 # Callback pour afficher les courbes lorsque le bouton est cliqué
-@app.callback(Output('preview-button', 'children'),
-              [Input('preview-button', 'n_clicks')],
-              [Input('date-picker-range', 'start_date'),
-               Input('date-picker-range', 'end_date')])
-def execute_command(n_clicks, start_date, end_date):
-    if n_clicks:
-        message = "Appel de la fonction C++"
-        dp.generate_output_json("./output.json")        
-        command = ["./hedging_portfolio", "../../front-python/output.json", "../../front-python/sortie.json"]
-        subprocess.run(command, cwd="../src/build")
-        return message + "\nPrévisualisation effectuée avec succès."
+# @app.callback(Output('preview-button', 'children'),
+#               [Input('preview-button', 'n_clicks')],
+#               [Input('date-picker-range', 'start_date'),
+#                Input('date-picker-range', 'end_date')])
+# def execute_command(n_clicks, start_date, end_date):
+#     if n_clicks:
+#         dp.generate_output_json("./output.json")        
+#         command = ["./hedging_portfolio", "../../front-python/output.json", "../../front-python/sortie.json"]
+#         subprocess.run(command, cwd="../src/build")
+#         return "Prévisualisation effectuée avec succès."
 
 # Définir la mise en page de la gestion de portefeuille
 def gestion_accueil_layout():
@@ -93,7 +106,7 @@ def gestion_accueil_layout():
                     className='mt-3'
                 ),
                 html.Button('Afficher les courbes', id='afficher-courbes-button', className='btn btn-primary mt-3'),
-                html.Button('Prévisualisation', id='preview-button', className='btn btn-secondary mt-3 ml-2')
+                html.Button('Prévisualisation', id='preview-button', className='btn btn-secondary mt-3', n_clicks=0)
             ]),
             # Div pour afficher les courbes (initiallement vide)
             html.Div(id='graph-container'),
@@ -103,29 +116,83 @@ def gestion_accueil_layout():
         ], className='content')
     ])
 
-@app.callback(Output('table-container', 'children'),
-              [Input('preview-button', 'n_clicks')])
+@app.callback(
+    Output('table-container', 'children'),
+    [Input('preview-button', 'n_clicks')]
+)
 def display_table(n_clicks):
     if n_clicks:
         try:
+            # Exécuter le programme externe
+            command = ["./hedging_portfolio", "../../front-python/output.json", "../../front-python/sortie.json"]
+            subprocess.run(command, cwd="../src/build")
+
+            # Charger les données à partir de sortie.json
             with open('./sortie.json', 'r') as f:
                 data = json.load(f)
-                return dash_table.DataTable(
-                    id='table',
-                    columns=[
-                        {'name': 'Date', 'id': 'date'},
-                        {'name': 'Deltas', 'id': 'deltas'},
-                        {'name': 'Deltas StdDev', 'id': 'deltasStdDev'},
-                        {'name': 'Price', 'id': 'price'},
-                        {'name': 'Price StdDev', 'id': 'priceStdDev'}
-                    ],
-                    data=data,
-                    page_size=10
-                )
+                data = pd.DataFrame(data)
+
+            # Extraire les données nécessaires
+            first_option_date = datetime.strptime(dataf._first_option_dates[0], "%d-%m-%Y")
+
+            # Convertir la date en chaîne de caractères dans le même format que l'index du DataFrame
+            first_option_date_str = first_option_date.strftime('%d-%m-%Y')
+
+            first_option_date_iso = first_option_date.strftime('%Y-%m-%dT00:00:00')
+
+            math_date = data['date']
+            
+            #on recupere la date de la premiere option en utilisant iloc
+            index_ligne = df.index.get_loc(first_option_date_is)
+            
+
+            index_ligne = index_ligne + math_date
+
+            date = df['Date'][index_ligne]
+            deltas = data[0]['deltas']
+            deltas_stddev = data[0]['deltasStdDev']
+            # Charger les données à partir de output.json
+            with open('./output.json', 'r') as f_output:
+                data_output = json.load(f_output)
+                asset_values = data_output['Past'][-1]
+
+            # Créer une liste pour stocker les données de la table
+            table_data = []
+
+            # Extraire les noms des actifs
+            assets = ["EUROSTOXX50", "MIB", "FTSE100", "NIKKEI", "SENSEX", "GBP", "JPY", "INR"]
+
+            # Parcourir les données et ajouter à table_data
+            for i in range(len(assets)):
+                entry = {
+                    'Date': date,
+                    'Asset': assets[i],
+                    'Asset Value': asset_values[i],
+                    'Delta': deltas[i],
+                    'Delta StdDev': deltas_stddev[i]
+                }
+                table_data.append(entry)
+
+            # Créer le DataFrame pour la table
+            df_table = pd.DataFrame(table_data)
+
+            return dash_table.DataTable(
+                id='table',
+                columns=[
+                    {'name': 'Date', 'id': 'Date'},
+                    {'name': 'Asset', 'id': 'Asset'},
+                    {'name': 'Asset Value', 'id': 'Asset Value'},
+                    {'name': 'Delta', 'id': 'Delta'},
+                    {'name': 'Delta StdDev', 'id': 'Delta StdDev'}
+                ],
+                data=df_table.to_dict('records'),
+                page_size=10
+            )
         except FileNotFoundError:
-            return "Le fichier sortie.json n'a pas été trouvé."
+            return "Le fichier sortie.json ou output.json n'a pas été trouvé."
         except Exception as e:
             return str(e)
+
 
 # Définir la mise en page de l'accueil
 def accueil_layout():
